4ef65da8e2bf433278baa238cc8dbd2034bb4363
Grace Ho <Grace Ho@LAPTOP-QR1O484K.lan>
2025-04-21 12:34:00 -0700

    please work

---
+++ src/testproj/TestTest.java
@@ -0,0 +1,41 @@
+/**
+* Lead Author(s):
+* @author Grace Ho; student ID
+* @author Full name; student ID
+* <<Add additional lead authors here>>
+*
+* Other Contributors:
+* Full name; student ID or contact information if not in class
+* <<Add additional contributors (mentors, tutors, friends) here, with contact information>>
+*
+* References:
+* Morelli, R., & Walde, R. (2016).
+* Java, Java, Java: Object-Oriented Problem Solving
+* https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
+*
+* <<Add more references here>>
+*
+* Version: 2025-04-21
+*/
+package testproj;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Purpose: The reponsibility of TestTest is ...
+ *
+ * TestTest is-a ...
+ * TestTest is ...
+ */
+class TestTest
+{
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
